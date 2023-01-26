b6ee0fcdc30b25ec41a9d6b021b4f88ca437f2fb
Anton Oboleninov <anton.oboleninov@saritasa.com>
2023-01-26 11:55:52 +0300

    Initial commit

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy.http import TextResponse
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'https://quotes.toscrape.com/page/1/',
+        'https://quotes.toscrape.com/page/2/',
+    ]
+
+    # Equal to `start_urls`
+    # def start_requests(self):
+    #     urls = [
+    #         'https://quotes.toscrape.com/page/1/',
+    #         'https://quotes.toscrape.com/page/2/',
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response: TextResponse):
+        """Parse quotes information: text, author and tags."""
+        # Default callback
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
